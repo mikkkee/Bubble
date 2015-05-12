a8a8e707ad9c4eae4b82f5b468b62630c3cbf8ad
mikkkee <boy.swordman@gmail.com>
2015-05-12 19:20:56 +0800

    pre_processing fix

--- pre_travis_ci.sh
+++ pre_travis_ci.sh
@@ -3,8 +3,8 @@
 # Change settings.py for testing.
 cp settings_test.py settings.py
 
-# Run by feeding test input for further testing.
-python run_sample.py
-
 # Empty output file names container.
 echo -n '' > names.txt
+
+# Run by feeding test input for further testing.
+python run_sample.py
